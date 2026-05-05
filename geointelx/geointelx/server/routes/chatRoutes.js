17a04b727962496f115e0ca9a9f9e300d6cc60da
swarada2407 <swarada.j2407@gmail.com>
2026-05-05 22:55:23 +0530

    Fix submodule and upload actual code

--- geointelx/geointelx/server/routes/chatRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { askGemini } from "../controllers/chatController.js";
-import { protect } from "../middleware/authMiddleware.js";
-
-const router = express.Router();
-
-router.post("/", askGemini);
-
-export default router;
